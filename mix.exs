63232102f1cf8449287cd313d2bc4f90b87b0ba9
Shih-Min Lee <ssmlee04@gmail.com>
2020-06-14 17:46:06 -0400

    elixir version 1.6

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ElixirAuthGoogle.MixProject do
     [
       app: :elixir_auth_google,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: @description,
